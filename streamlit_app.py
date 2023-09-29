78c1833c77ea477802274fcd769cdaf17b77da21
YKKaya <73110464+YKKaya@users.noreply.github.com>
2023-09-29 18:39:18 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,38 +1,74 @@
-from collections import namedtuple
-import altair as alt
-import math
-import pandas as pd
-import streamlit as st
+# Function to fetch S&P 500 data
+def fetch_sp500_data(url):
+    try:
+        tickers = pd.read_html(url)[0]
+        return tickers
+    except Exception as e:
+        st.error(f"Error fetching S&P 500 data: {e}")
+        return None
 
-"""
-# Welcome to Streamlit!
+# Function to download stock data
+def download_stock_data(Stocks):
+    try:
+        Portfolio = yf.download(Stocks , period='1y', interval='1h')
+        return Portfolio
+    except Exception as e:
+        st.error(f"Error downloading stock data: {e}")
+        return None
 
-Edit `/streamlit_app.py` to customize this app to your heart's desire :heart:
+# Function to process data
+def process_data(Portfolio):
+    try:
+        portfolio = Portfolio.stack().reset_index().rename(index=str, columns={"level_1": "Symbol", "level_0": "Datetime"})
+        portfolio['Return'] = (portfolio['Close'] - portfolio['Open']) / portfolio['Open']
+        return portfolio
+    except Exception as e:
+        st.error(f"Error processing data: {e}")
+        return None
 
-If you have any questions, checkout our [documentation](https://docs.streamlit.io) and [community
-forums](https://discuss.streamlit.io).
+# Function to merge additional info
+def merge_additional_info(portfolio, tickers):
+    try:
+        company_info = tickers[['Symbol', 'Security', 'GICS Sector', 'GICS Sub-Industry', 'Headquarters Location', 'Date added', 'Founded']]
+        company_info.columns = ['Symbol', 'Company_Name', 'Industry', 'Sub_Industry', 'Headquarters_Location', 'Date_Added', 'Founded']
+        portfolio = pd.merge(portfolio, company_info, on='Symbol', how='left')
+        return portfolio
+    except Exception as e:
+        st.error(f"Error merging additional info: {e}")
+        return None
 
-In the meantime, below is an example of what you can do with just a few lines of code:
-"""
+# Title and Description
+st.title("S&P 500 Analysis")
+st.write("""
+An interactive analysis of S&P 500 companies, allowing users to view historical stock data, returns, and additional company information.
+""")
 
+# URL for fetching S&P 500 companies data
+url = 'https://en.wikipedia.org/wiki/List_of_S%26P_500_companies'
 
-with st.echo(code_location='below'):
-    total_points = st.slider("Number of points in spiral", 1, 5000, 2000)
-    num_turns = st.slider("Number of turns in spiral", 1, 100, 9)
+# Fetching the list of S&P 500 companies from Wikipedia
+tickers = fetch_sp500_data(url)
 
-    Point = namedtuple('Point', 'x y')
-    data = []
+# Selecting the ticker symbols
+Stocks = tickers.Symbol.to_list()
 
-    points_per_turn = total_points / num_turns
+# Downloading historical stock data
+Portfolio = download_stock_data(Stocks)
 
-    for curr_point_num in range(total_points):
-        curr_turn, i = divmod(curr_point_num, points_per_turn)
-        angle = (curr_turn + 1) * 2 * math.pi * i / points_per_turn
-        radius = curr_point_num / total_points
-        x = radius * math.cos(angle)
-        y = radius * math.sin(angle)
-        data.append(Point(x, y))
+# Processing the stock data
+portfolio = process_data(Portfolio)
 
-    st.altair_chart(alt.Chart(pd.DataFrame(data), height=500, width=500)
-        .mark_circle(color='#0068c9', opacity=0.5)
-        .encode(x='x:Q', y='y:Q'))
+# Merging additional company information
+portfolio = merge_additional_info(portfolio, tickers)
+
+# Cleaning the 'Founded' column by removing year values wrapped in parentheses
+if portfolio is not None:
+    portfolio['Founded'] = portfolio['Founded'].str.replace(r'\(.*?\)', '', regex=True).str.strip()
+
+# Calculating the dollar value of the return
+portfolio['Dollar_Return'] = portfolio['Return'] * portfolio['Adj Close']
+
+# Display Results
+st.write("### Data Table:")
+# Display the processed data in a table format
+st.dataframe(portfolio)
